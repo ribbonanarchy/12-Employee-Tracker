5791149bfde1b40d5d51db0fe222accd48280a64
Robyn Arnecke <robynarnecke@gmail.com>
2022-03-02 19:32:04 -0600

    Started writing inquirer prompts, startPrompt functional

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const inquirer = require('inquirer');
 const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
@@ -20,3 +21,40 @@ const db = mysql.createConnection(
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+function startPrompt() {
+    inquirer.prompt([
+        {
+            type: 'list', 
+            name: 'choice',
+            message: 'Which action would you like to take?', 
+            choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add a Department', 'Add a Role', 'Add an Employee', 'Update an Employee Role']
+        }
+    ]).then((userAnswers) => {
+        switch(userAnswers.choice) {
+            case 'View All Departments': 
+                console.log(userAnswers.choice);
+                break;
+            case 'View All Roles': 
+                console.log(userAnswers.choice);
+                break;
+            case 'View All Employees': 
+                console.log(userAnswers.choice);
+                break;
+            case 'Add a Department': 
+                console.log(userAnswers.choice);
+                break;
+            case 'Add a Role':
+                console.log(userAnswers.choice);
+                break;
+            case 'Add an Employee':
+                console.log(userAnswers.choice);
+                break;
+            case 'Update an Employee Role':
+                console.log(userAnswers.choice);
+                break;
+        }
+    })
+}
+
+startPrompt();
